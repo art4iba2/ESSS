2b737f4521a43d142d000d77f187c808ecf0b2cf
art4i <agg0073@gmail.com>
2023-06-11 23:44:48 +0400

    Добавьте файлы проекта.

---
+++ WebApplication15/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication15.Models;
+namespace WebApplication15.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            student model = new student();
+            model.name = " ";
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult PostMethod(student model)
+        {
+            HttpCookie cookie = new HttpCookie("name", model.name);
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Show");
+        }
+
+        public ActionResult Show()
+        {
+            HttpCookie cookie = Request.Cookies["name"];
+            string show = cookie != null ? cookie.Value : null;
+
+            ViewBag.show = show;
+
+            return View();
+        }
+    }
+}
